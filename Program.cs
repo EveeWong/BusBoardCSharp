d0fe15fe6f4aad66fc4a6f000618ba531aa15a53
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-03-05 17:10:58 +0000

    Take in a postcode from user input

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace BusBoardCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a valid London postcode:");
+            string postcode = Console.ReadLine();
+
+
+        }
+
+        public void ValidatePostcode(string postcode) 
+        {
+            
         }
     }
 }
